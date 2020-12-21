18ba4bed8e2c983021a825a6be76bf8f25b745f1
voxal <aidenhohoho@gmail.com>
2020-12-20 19:45:41 -0800

    More quotes; Gallery framework; Credits now send to new tab

--- scripts/credits.js
+++ scripts/credits.js
@@ -1,23 +1,27 @@
-let container = document.getElementById("credits");
-data.credits.forEach((credit) => {
-  let wrapper = document.createElement("div");
-  wrapper.innerHTML = `
+(() => {
+  let container = document.getElementById("credits");
+  data.credits.forEach((credit) => {
+    let wrapper = document.createElement("div");
+    wrapper.innerHTML = `
   <a ${credit.link ? `href="${credit.link}"` : ""} 
   class="credit"
   ${
     data.creditColors[credit.role]
       ? `style="color:${data.creditColors[credit.role]}"`
       : ""
-  }>
+  }
+  target="_blank"
+  >
     <br>
     <h3 style="text-decoration: ${credit.link ? "underline" : "none"};">${
-    credit.name
-  }</h3>
+      credit.name
+    }</h3>
     ${credit.role}
     <br>
     <i>${credit.quote || "Too wuss to have a quote"}</i>
     <br>
   <a>
     `;
-  container.appendChild(wrapper);
-});
+    container.appendChild(wrapper);
+  });
+})();
---
+++ scripts/gallery.js
@@ -0,0 +1,11 @@
+class Carousel {
+  constructor(dataSet, element) {
+      this.data = dataSet;
+      this.element = document.getElementById(element);
+      for(const i in this.data){
+        let slide = document.createElement("div");
+        
+      }
+      
+  }
+}
