583a2e2545aab4eead43a4cc048a7bdf85ecc1f7
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2021-05-05 20:55:51 -0400

    sowing the seeds for a future site easter egg...
    COME THE HECK ON DAEDALUS FINISH THE ASSET ALREADY

--- scripts/home.js
+++ scripts/home.js
@@ -1,17 +1,22 @@
 (() => {
-  let entry =
-    data.header.subtitle[
-      Math.floor(Math.random() * data.header.subtitle.length)
-    ];
-  if (typeof entry == "string")
-    document.getElementById("headerSubtitle").innerHTML = entry;
-  if (typeof entry == "function") entry();
+  let entry = data.header.subtitle[Math.floor(Math.random() * data.header.subtitle.length)];
+  if (typeof entry == "string"){document.getElementById("headerSubtitle").innerHTML = entry;}
+  if (typeof entry == "function"){entry();}
+  if(entry === "There are currently quotes (including this one) on the site. Reload to see if your submission was one of them!"){document.getElementById("headerSubtitle").innerHTML = `There are currently ${data.header.subtitle.length} quotes (including this one) on the site. Reload to see if your submission was one of them!`;}
+  //if (Math.random() < 0.01) {document.getElementById("logo").src = "assets/logo.gif";} else {document.getElementById("logo").src = "assets/amongus.gif";}
   let full = document.getElementById("fullLink");
   let overlay = document.getElementById("overlayLink");
   full.href = data.downloads[0].fileFull;
   full.innerHTML = `${data.downloads[0].version} Full (Latest)`;
   overlay.href = data.downloads[0].fileOverlay;
   overlay.innerText = `${data.downloads[0].version} Overlay (Latest)`;
+  console.log(`
+    Hi.
+    
+    Alt+F4 for a free Necron's Handle. C'mon, we know you want it.
+
+    OK, fine, you called our bluff. Now quit poking around here and go back to finding some funny little quote to screenshot.
+  `);
   if (window.navigator.userAgent.indexOf("MSIE ") > 0)
     document.getElementById("headerSubtitle").innerHTML =
       "internet explorer is trash use another browser pls";
