a5b2a85adbaec025c1fd735481cf7f6716ba66f4
voxal <aidenhohoho@gmail.com>
2021-05-11 12:27:26 -0700

    Currently only shows swords, will work when i get back home

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,13 @@
 const tabs = {
   armor: "/assets/gallery/armor/armor.png",
   swords: "/assets/gallery/swords/swords.png",
-  bows:"/assets/gallery/bows/bows.png"
-}
+  bows: "/assets/gallery/bows/bows.png",
+  tools: "/assets/gallery/tools/tools.png",
+  talismans: "/assets/gallery/talismans/talismans.png",
+  reforges: "/assets/gallery/reforges/reforges.png",
+  misc: "/assets/gallery/misc/misc.png",
+};
+
+const switchTab = (tabName) => {
+  const tabby = document.getElementById("")
+};
