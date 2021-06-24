5989515591238b1fad42110e873dc69645018225
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2021-06-23 22:14:29 -0400

    and part 3

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,5 @@
 const date = new Date();
+const isMobile = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)));
 const pressToMoveQuotes = (quotes) => {
   let quoteIndex = -1;
   const quote = document.getElementById("headerSubtitle");
@@ -1807,6 +1808,53 @@ const data = {
       linkText: `Visit r/HypixelSkyblock here.`,
     },
   ],
+  steps: [
+    {
+      step: `Download FurfSky Reborn.<br><a href="/">Latest version</a> (recommended)<br><a href="/downloads/">All versions</a>`,
+      type: `Required`,
+      title: `How else are you going to use FurfSky Reborn?`,
+    },
+    {
+      step: `If you haven't done so already, download and install OptiFine for 1.8.9.<br>macOS players: <a href="https://optifine.net/adloadx?f=preview_OptiFine_1.8.9_HD_U_M6_pre1.jar">Optifine M6</a> Windows and Linux players: <a href="https://optifine.net/adloadx?f=OptiFine_1.8.9_HD_U_M5.jar">OptiFine M5</a>`,
+      type: `Required`,
+      title: `OptiFine is required to apply textures to non-vanilla items such as the ones you see in Hypixel Skyblock.`,
+    },
+    {
+      step: `Launch Minecraft and from the splash screen/main menu, go to your resource packs.<br>(<code>Options > Resource Packs</code>)`,
+      type: `Required`,
+      title: `This is how you navigate to your resources packs menu.`,
+    },
+    {
+      step: `Click the "Open resource pack folder" button towards the bottom left of the screen.`,
+      type: `Required`,
+      title: `If you have your own method of finding your texture packs folder, go for it. This step is intended to be a cross-platform method.`,
+    },
+    {
+      step: `Drag the FurfSky Reborn <code>.zip</code> file from your downloads folder into the resource packs folder.`,
+      type: `Required`,
+      title: `This is how you get Minecraft to acknowledge the Furfsky Reborn pack.`,
+    },
+    {
+      step: `Move the FurfSky Reborn texture pack from the left column into the right column.<br>Then press "Done" towards the bottom right.`,
+      type: `Required`,
+      title: `Again, this is a cross-platform step. If you already know how to apply texture packs or have your own methods, use those.`,
+    },
+    {
+      step: `Make sure Custom Textures are enabled by going to<br><code>Video Settings > Quality > Custom Items: ON</code> and enabling it.`,
+      type: `Required`,
+      title: `This is the second part of using OptiFine to apply custom item textures.`,
+    },
+    {
+      step: `Disable Fast Render.<br>(<code>Video Settings > Performance > Fast Render: OFF</code>)`,
+      type: `Required`,
+      title: `Fast Render causes problems as your Hypixel Skyblock mods library grows, and this step is a precautionary measure in minimizing problems in the future.`,
+    },
+    {
+      step: `Enable custom texture's animations by going to<br><code>Video Settings > Animations > Terrain Animated: ON</code>.`,
+      type: `Optional`,
+      title: `If you don't want animations for your items, you can turn this option "OFF" instead.`,
+    },
+  ]
 };
 
 //inject navbar
