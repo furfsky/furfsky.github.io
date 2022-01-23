c1cb5189149348975182a234c5e2c38e211c8c58
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2022-01-23 12:28:11 -0500

    🐱 (papaya) new trial + comment blocks for giggles

--- scripts/credits/credits.config.js
+++ scripts/credits/credits.config.js
@@ -199,6 +199,23 @@ export const credits = [
     quote: "How do i animate stuff minecraft."
   },
   {
+    name: "Papaya",
+    role: "Trial Artist",
+    quote: "🐱"
+  },
+  /*
+  {
+    name: "zburger",
+    role: "Trial Artist",
+    quote: "ew burger ice cream"
+  },
+  {
+    name: "TempDialga",
+    role: "Trial Artist",
+    quote: "With great value comes an intermediate degree of price dependent upon the supply"
+  },
+  */
+  {
     name: "<span style='background: -webkit-linear-gradient(top left, rgba(191,97,106,1) 0%, rgba(191,97,106,1) 20%, rgba(208,135,112,1) 20%, rgba(208,135,112,1) 35%, rgba(235,203,139,1) 35%, rgba(235,203,139,1) 50%, rgba(163,190,140,1) 50%, rgba(163,190,140,1) 65%, rgba(136,192,208,1) 65%, rgba(136,192,208,1) 80%, rgba(180,142,173,1) 80%, rgba(180,142,173,1) 100%); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>voxal</span>",
     link: "https://thoughtscape.vercel.app/",
     role: "<span style='background: -webkit-linear-gradient(top left, rgba(191,97,106,1) 0%, rgba(191,97,106,1) 20%, rgba(208,135,112,1) 20%, rgba(208,135,112,1) 35%, rgba(235,203,139,1) 35%, rgba(235,203,139,1) 50%, rgba(163,190,140,1) 50%, rgba(163,190,140,1) 65%, rgba(136,192,208,1) 65%, rgba(136,192,208,1) 80%, rgba(180,142,173,1) 80%, rgba(180,142,173,1) 100%); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>Website Developer</span>",
