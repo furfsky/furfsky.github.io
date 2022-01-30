f2bb41df0366e57c1a2431d4543190b07bb22f28
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-01-30 09:36:43 -0500

    updated gallery to v1.3.2

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,11 +1,12 @@
 const tabs = {
-  armor: '/assets/gallery/armor/armor.png',
   swords: '/assets/gallery/swords/swords.png',
-  bows: '/assets/gallery/bows/bows.png',
+  armor: '/assets/gallery/armor/armor.png',
+  bows: '/assets/gallery/bows/bows.gif',
   tools: '/assets/gallery/tools/tools.png',
   talismans: '/assets/gallery/talismans/talismans.png',
   reforges: '/assets/gallery/reforges/reforges.png',
   misc: '/assets/gallery/misc/misc.png',
+  changelog: '/assets/gallery/changelogs/1.3.2/1.3.2.gif',
 };
 
 const switchTab = (tabName) => {
