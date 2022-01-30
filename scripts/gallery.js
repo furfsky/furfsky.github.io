476df6ff75b8f0b1dc3fdf4d43a085dc81f41b3a
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-01-30 11:02:00 -0500

    removed unneeded gallery files

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,12 +1,11 @@
 const tabs = {
-  swords: '/assets/gallery/swords/swords.png',
-  armor: '/assets/gallery/armor/armor.png',
-  bows: '/assets/gallery/bows/bows.gif',
-  tools: '/assets/gallery/tools/tools.png',
-  talismans: '/assets/gallery/talismans/talismans.png',
-  reforges: '/assets/gallery/reforges/reforges.png',
-  misc: '/assets/gallery/misc/misc.png',
-  // changelog: '/assets/gallery/changelogs/1.3.2/1.3.2.gif',
+  swords: '/assets/gallery/swords.png',
+  armor: '/assets/gallery/armor.png',
+  bows: '/assets/gallery/bows.gif',
+  tools: '/assets/gallery/tools.png',
+  talismans: '/assets/gallery/talismans.png',
+  reforges: '/assets/gallery/reforges.png',
+  misc: '/assets/gallery/misc.png',
 };
 
 const switchTab = (tabName) => {
